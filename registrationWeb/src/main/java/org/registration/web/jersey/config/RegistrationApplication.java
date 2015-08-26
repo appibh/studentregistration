76c6a9caebc2e117242d0626b14d09f75e79fbca
appibh <appi.bh@gmail.com>
2015-08-26 16:35:26 -0500

    Removed the resource registration as it is not required

--- registrationWeb/src/main/java/org/registration/web/jersey/config/RegistrationApplication.java
+++ registrationWeb/src/main/java/org/registration/web/jersey/config/RegistrationApplication.java
@@ -3,12 +3,10 @@ package org.registration.web.jersey.config;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
-import org.registration.web.resource.StudentResource;
 
 public class RegistrationApplication extends ResourceConfig {
 	public RegistrationApplication(){
 		register(RequestContextFilter.class);
-		register(StudentResource.class);
 		register(JacksonFeature.class);	
 	}
 
